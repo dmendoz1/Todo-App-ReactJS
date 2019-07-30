071615ebe4d18e48eff49d956437a915b348555a
Daniel Mendoza <dmendoza@fb.com>
2019-07-30 10:30:31 -0700

    Updates

---
+++ precache-manifest.24fe66846cd9e7d78d9e6c3495a9175a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937ec682cea7a3c61ec4352fc0cd7ba0",
+    "url": "/todo-app-reactJS/index.html"
+  },
+  {
+    "revision": "a5448a20d619bb90a902",
+    "url": "/todo-app-reactJS/static/css/main.f78400bb.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/todo-app-reactJS/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "a5448a20d619bb90a902",
+    "url": "/todo-app-reactJS/static/js/main.26c242e0.chunk.js"
+  },
+  {
+    "revision": "d9eb226a63b5da265198",
+    "url": "/todo-app-reactJS/static/js/runtime~main.9ec112e0.js"
+  }
+]);
